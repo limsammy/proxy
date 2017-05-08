21fc42655beff58f7c0dfe2e31ccc04e46e310f7
Sam Lim <limsammy1@gmail.com>
2017-05-08 09:29:36 -0600

    added removes newline spec

--- checker/spec/check_spec.rb
+++ checker/spec/check_spec.rb
@@ -9,4 +9,10 @@ describe ProxyCheck do
     expect(@proxy.to_check).to be_instance_of(Array)
     expect(@proxy.to_check.count).to eq(18)
   end
+
+  it 'removes newline char from to_check array' do
+    @proxy.to_check.each do |proxy|
+      expect(proxy).to_not include('\n')
+    end
+  end
 end
