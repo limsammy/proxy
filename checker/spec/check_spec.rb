9f3cc817c9dcac59b4f3d8d873e6eb692264937d
Sam Lim <limsammy1@gmail.com>
2017-05-08 09:20:10 -0600

    added first test for reading file into array

--- checker/spec/check_spec.rb
+++ checker/spec/check_spec.rb
@@ -4,4 +4,8 @@ describe ProxyCheck do
   before(:each) do
     @proxy = ProxyCheck.new('./to_check.txt')
   end
+
+  it 'reads file into array' do
+    expect(@proxy.to_check).to be_instance_of(Array)
+  end
 end
